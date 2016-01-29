c145bf6a999c13d12199dcadb61ffbe0f0b166fb
Juan Pumarino <juan@scribd.com>
2016-01-28 16:24:03 -0800

    Handle both Local Control and Step behavior

--- main.go
+++ main.go
@@ -12,63 +12,86 @@ import (
 )
 
 const (
-	ccMessage      = 0xB0 // Table 2: Chan 1 Control/Mode Change in http://www.midi.org/techspecs/midimessages.php
-	localControlCC = 0x7A // Table 3: Local Control On/Off (supported by MicroBrute)
-	onMessage      = 127
-	offMessage     = 0
+	ccMessage             = 0xB0 // Table 2: Chan 1 Control/Mode Change in http://www.midi.org/techspecs/midimessages.php
+	localControlCCNumber  = 0x7A // Table 3: Local Control On/Off (supported by MicroBrute)
+	stepControlCCNumber   = 0x72 // Undefined by MIDI: MB uses it for Step On Clk | Gate
+	localControlOnValue   = 127
+	localControlOffValue  = 0
+	stepControlGateValue  = 127
+	stepControlClockValue = 0
 )
 
 func main() {
 
 	var (
-		localControlVal  int64
-		selectedDeviceId portmidi.DeviceId = -1
+		ccValue  int64
+		ccNumber int64
+		deviceId portmidi.DeviceId = -1
+		err      error
 	)
 
-	if len(os.Args) != 2 {
-		fmt.Println("No argument provided")
+	if len(os.Args) != 3 {
+		fmt.Println("Not enough argument provided: (local|step) (on|off|clock|gate)")
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
-	case "on":
-		localControlVal = onMessage
-	case "off":
-		localControlVal = offMessage
+	case "local":
+		ccNumber = localControlCCNumber
+	case "step":
+		ccNumber = stepControlCCNumber
 	default:
-		fmt.Println("Argument must be 'on' or 'off'")
+		fmt.Println("Operation must be (local|step)")
+		os.Exit(1)
+	}
+
+	switch os.Args[2] {
+	case "on":
+		ccValue = localControlOnValue
+	case "off":
+		ccValue = localControlOffValue
+	case "clock":
+		ccValue = stepControlClockValue
+	case "gate":
+		ccValue = stepControlGateValue
+	default:
+		fmt.Println("Value must be (on|off|clock|gate)")
 		os.Exit(1)
 	}
 
 	portmidi.Initialize()
 	defer portmidi.Terminate()
+
+	deviceId, err = getDeviceId()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	out, err := portmidi.NewOutputStream(deviceId, 1024, 0)
+	if err != nil {
+		fmt.Println("Error opening MicroBrute output port:", err)
+		os.Exit(1)
+	}
+
+	err = out.WriteShort(ccMessage, ccNumber, ccValue)
+	if err != nil {
+		fmt.Println("Error sending message to MicroBrute:", err)
+		os.Exit(1)
+	}
+
+}
+
+func getDeviceId() (portmidi.DeviceId, error) {
 	total := portmidi.CountDevices()
 
 	for id := 0; id < total; id++ {
 		id := portmidi.DeviceId(id)
 		info := portmidi.GetDeviceInfo(id)
 		if info.Name == "MicroBrute" && info.IsOutputAvailable {
-			selectedDeviceId = id
-			fmt.Println("Found MicroBrute output port at device id", selectedDeviceId)
-			break // Only apply command to first MicroBrute found
+			return id, nil
 		}
 	}
 
-	if selectedDeviceId == -1 {
-		fmt.Println("MicroBrute not found")
-		os.Exit(1)
-	}
-
-	out, err := portmidi.NewOutputStream(selectedDeviceId, 1024, 0)
-	if err != nil {
-		fmt.Println("Error opening MicroBrute output port:", err)
-		os.Exit(1)
-	}
-
-	err = out.WriteShort(ccMessage, localControlCC, localControlVal)
-	if err != nil {
-		fmt.Println("Error sending message to MicroBrute:", err)
-		os.Exit(1)
-	}
-
+	return -1, fmt.Errorf("MicroBrute not found")
 }
